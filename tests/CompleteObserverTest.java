1e286e8ac92c9b2bf583d02e0a8fddedaf901ba7
Anna-8 <anna.valanzano@stud.unifi.it>
2020-08-27 11:55:16 +0200

    final test

---
+++ tests/CompleteObserverTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CompleteObserverTest {
+    @Test
+    public void testObserver(){
+        Order order = new Order();
+        CompleteObserver observer = new CompleteObserver();
+        order.addObserver(observer);
+        observer.setSubject(order);
+        order.makeDrink();
+        assertEquals(observer.isReadyDrink(), order.isReadyDrink());
+
+    }
+}
